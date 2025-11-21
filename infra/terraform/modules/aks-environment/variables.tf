c3fe0a5ca8d7eae081e6dc0fb366c90dd208ec98
isabelaocampos <isabelaocampo08@gmail.com>
2025-11-20 20:08:14 -0500

    feat: Refactor Terraform configuration for AKS and ACR setup, remove obsolete variables and outputs

---
+++ infra/terraform/modules/aks-environment/variables.tf
@@ -0,0 +1,26 @@
+variable "env_name" {
+  description = "Nombre del ambiente (ej: dev, stage, prod) para sufijar recursos"
+  type        = string
+}
+
+variable "location" {
+  description = "Región de Azure"
+  type        = string
+  default     = "East US"
+}
+
+variable "node_count" {
+  description = "Número de nodos en el pool por defecto"
+  type        = number
+}
+
+variable "vm_size" {
+  description = "Tamaño de la VM para los nodos"
+  type        = string
+  default     = "Standard_B4ms"
+}
+
+variable "acr_base_name" {
+  description = "Nombre base para el Azure Container Registry"
+  type        = string
+}
